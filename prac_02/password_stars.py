c32550e34204c7e9c2df9ee05ad9b104fb6b1135
ejm28 <eddie.morse@my.jcu.edu.au>
2025-05-31 10:24:31 +1000

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,26 @@
+"""
+Password function
+"""
+
+
+def main():
+    password = get_valid_password()
+    print_hidden_password(password)
+
+
+def get_valid_password():
+    """Return a valid password."""
+    password = input("Enter password: ")
+    while len(password) < 8:
+        print("Password must be at least 8 characters long")
+        password = input("Enter password: ")
+    return password
+
+
+def print_hidden_password(password):
+    """Print hidden password of length asterisk"""
+    length = len(password)
+    print("*" * length)
+
+
+main()
