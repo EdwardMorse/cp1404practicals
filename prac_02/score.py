dcbc9f1d0f8c4589a6aa235231c8a0cf1529ff5a
ejm28 <eddie.morse@my.jcu.edu.au>
2025-06-01 10:21:47 +1000

    Add the random score generation program

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,15 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+import random
+
 
 def main():
     """Get the valid score and display the result."""
     score = float(input("Enter score: "))
     print(determine_score_result(score))
-
+    random_score = random.randint(0, 100)
+    print(f"The random score of {random_score} is {determine_random_score(random_score)}.")
 
 def determine_score_result(score):
     """Determine the result category of the valid score."""
@@ -18,6 +21,16 @@ def determine_score_result(score):
     if score >= 90:
         return "Excellent"
     elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def determine_random_score(random_score):
+    """Return a random valid score."""
+    if random_score >= 90:
+        return "Excellent"
+    elif random_score >= 50:
         return "Passable"
     else:
         return "Bad"
