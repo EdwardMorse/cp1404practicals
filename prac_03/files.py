a35b8d04c72ed01c9a99671f035876b797a7edd4
ejm28 <eddie.morse@my.jcu.edu.au>
2025-06-08 15:15:04 +1000

    Add password checker and files programs

---
+++ prac_03/files.py
@@ -0,0 +1,30 @@
+"""
+Files program: Using different techniques to read files
+"""
+
+FILENAME = "name.txt"
+# 1.
+out_file = open(FILENAME, "w")
+name = input("What is your name? ")
+print(f"{name}", file=out_file)
+out_file.close()
+
+# 2.
+in_file = open(FILENAME, "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Hi {name}!")
+
+# 3.
+with open("numbers.txt", "r") as in_file:
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+print(number1 + number2)
+
+# 4.
+total = 0
+with open("numbers.txt", "r") as in_file:
+    for line in in_file:
+        number = int(line)
+        total += number
+print(total)
