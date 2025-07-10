58af660bc84d8922f2d9f92b23f166edb47b1948
ejm28 <eddie.morse@my.jcu.edu.au>
2025-07-11 09:05:24 +1000

    Add the programming languages class program and the languages client program

---
+++ prac_06/programming_language.py
@@ -0,0 +1,22 @@
+"""
+CP1404 Practical - programming language class file.
+Estimated time - 20 mins
+Actual time - 30 mins
+"""
+
+
+class ProgrammingLanguage:
+    """Programming language class."""
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Determine if a program is dynamic."""
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
