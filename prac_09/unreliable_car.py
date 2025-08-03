adcc55c29944521165e752bc75b47cdcb7452313
ejm28 <eddie.morse@my.jcu.edu.au>
2025-08-03 14:48:45 +1000

    Add the unreliable car and testing program

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,22 @@
+"""
+CP1404 Practical
+Unreliable Car class
+"""
+
+from prac_09.car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+    """Represent an unreliable car."""
+    def __init__(self, name, fuel, reliability):
+        """Initialize an unreliable car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Attempt to drive the unreliable car a certain distance."""
+        random_number = randint(0, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        return super().drive(distance)
---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,21 @@
+"""
+CP1404 Practical
+Unreliable Car Test
+"""
+
+from prac_09.unreliable_car import UnreliableCar
+
+def main():
+    """Test unreliable cars."""
+    good_car = UnreliableCar("Toyota", 100, 90)
+    bad_car = UnreliableCar("Ford", 100, 10)
+    for i in range(1, 12):
+        print(f"Attempting to drive {i}km:")
+        print(f"{good_car.name:12} drove {good_car.drive(i):2}km")
+        print(f"{bad_car.name:12} drove {bad_car.drive(i):2}km")
+
+    print(good_car)
+    print(bad_car)
+
+
+main()
